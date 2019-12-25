bf213b496c62c7a13c7bdcc68b578851a8508406
chunqiu-lz <2924592783@qq.com>
2019-12-25 22:03:58 +0800

    WelcomeActivity跳转到MainActivity

--- app/src/main/java/com/lzacking/wanandroid/activity/WelcomeActivity.java
+++ app/src/main/java/com/lzacking/wanandroid/activity/WelcomeActivity.java
@@ -1,14 +1,27 @@
 package com.lzacking.wanandroid.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.lzacking.wanandroid.R;
 
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    @Bind(R.id.iv_welcome_icon)
+    ImageView ivWelcomeIcon;
+    @Bind(R.id.rl_welcome)
+    RelativeLayout rlWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +31,29 @@ public class WelcomeActivity extends AppCompatActivity {
         // 隐藏顶部的状态栏
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+        // 提供启动动画
+        setAnimation();
     }
+
+    private Handler handler = new Handler();
+
+    private void setAnimation() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+        alphaAnimation.setDuration(3000);
+        alphaAnimation.setInterpolator(new AccelerateInterpolator());
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+        // 启动动画
+        rlWelcome.startAnimation(alphaAnimation);
+    }
+
 }
