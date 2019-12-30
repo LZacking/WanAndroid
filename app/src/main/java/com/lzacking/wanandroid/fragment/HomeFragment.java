252eaaba56b830e88ec1b48a8b1094f0c49f0b56
chunqiu-lz <2924592783@qq.com>
2019-12-30 22:11:16 +0800

    为各个页面添加页面头

--- app/src/main/java/com/lzacking/wanandroid/fragment/HomeFragment.java
+++ app/src/main/java/com/lzacking/wanandroid/fragment/HomeFragment.java
@@ -4,18 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.lzacking.wanandroid.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class HomeFragment extends Fragment {
+    @Bind(R.id.iv_title_back)
+    ImageView ivTitleBack;
+    @Bind(R.id.tv_title)
+    TextView tvTitle;
+    @Bind(R.id.iv_title_setting)
+    ImageView ivTitleSetting;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = View.inflate(getActivity(), R.layout.fragment_home, null);
+        ButterKnife.bind(this, view);
+        // 初始化title
+        initTitle();
         return view;
+    }
+
+    private void initTitle() {
+        ivTitleBack.setVisibility(View.GONE);
+        tvTitle.setText("首页");
+        ivTitleSetting.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
